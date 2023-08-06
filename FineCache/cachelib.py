9a63fef2056c564839b0be7246557cfb74924cf1
ciaranchen <ciaranchen@qq.com>
2023-08-06 08:58:10 +0800

    1.使用__qualname__; 2.分离Config类型

--- FineCache/cachelib.py
+++ FineCache/cachelib.py
@@ -24,8 +24,8 @@ class BaseCache:
     args_hash: Tuple[Callable] = None
     kwargs_hash: Dict[str, Callable] = None
 
-    @wraps
     def __call__(self, func):
+        @wraps(func)
         def _get_result(*args, **kwargs):
             call = CachedCall(func, args, kwargs)
             if self.exists(call):
@@ -46,27 +46,14 @@ class BaseCache:
         pass
 
 
-class PickleCache(BaseCache):
-    def __init__(self, base_path=None, cfg_path=None):
-        """
+@dataclass
+class FilenameConfig:
+    join_args: str = ';'
+    join_dict: str = ';'
+    join_key_value: str = '-'
+    template_string: str = "{func_name}@[{args}]@{kwargs}"
 
-        :param base_path: 保存的文件夹，默认为当前文件夹。
-        :param cfg_path: 配置文件的路径，主要用于指定保存文件的路径格式。
-        """
-        self.base_path = base_path if base_path else os.path.abspath(os.getcwd())
-        # default setting
-        self.join_args = ';'
-        self.join_dict = ';'
-        self.join_key_value = '-'
-        self.template_string = "{func_name}@[{args}]@{kwargs}"
-        # Load setting
-        if not cfg_path or not os.path.exists(cfg_path):
-            cfg_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '.cache_config.json')
-        with open(cfg_path) as cfg_fp:
-            cfg = json.load(cfg_fp)
-        self._load_config(cfg)
-
-    def _load_config(self, cfg):
+    def load(self, cfg: Dict[str, str]):
         if 'join_args' in cfg:
             self.join_args = cfg['join_args']
         if 'join_dict' in cfg:
@@ -76,7 +63,7 @@ class PickleCache(BaseCache):
         if 'template_string' in cfg:
             self.template_string = cfg['template_string']
 
-    def _get_id(self, call: CachedCall) -> str:
+    def get_id(self, call: CachedCall):
         """
         获取缓存的文件名
         :param call:
@@ -85,7 +72,33 @@ class PickleCache(BaseCache):
         arg_list, kwarg_list = call.arg_lst, call.kwarg_lst
         args_string = self.join_args.join(arg_list)
         kwargs_string = self.join_dict.join([k + self.join_key_value + v for k, v in kwarg_list.items()])
-        return self.template_string.format(func_name=call.func.__name__, args=args_string, kwargs=kwargs_string)
+        return self.template_string.format(func_name=call.func.__qualname__, args=args_string, kwargs=kwargs_string)
+
+
+class PickleCache(BaseCache):
+    def __init__(self, base_path=None, cfg_path=None):
+        """
+
+        :param base_path: 保存的文件夹，默认为当前文件夹。
+        :param cfg_path: 配置文件的路径，主要用于指定保存文件的路径格式。
+        """
+        self.base_path = base_path if base_path else os.path.abspath(os.getcwd())
+        # Load setting
+        self.config = FilenameConfig()
+
+        # scan path exist:
+
+        cfg_path = cfg_path if cfg_path else '.cache_config.json'
+        self.config_path = None
+        if cfg_path and os.path.exists(cfg_path):
+            self.config_path = cfg_path
+        elif cfg_path and base_path and os.path.exists(os.path.join(base_path, cfg_path)):
+            self.config_path = os.path.join(base_path, cfg_path)
+
+        if self.config_path:
+            with open(self.config_path) as config_fp:
+                cfg = json.load(config_fp)
+            self.config.load(cfg)
 
     @staticmethod
     def is_picklable(obj: Any) -> bool:
@@ -106,7 +119,7 @@ class PickleCache(BaseCache):
         :param call:
         :return:
         """
-        filename = os.path.join(self.base_path, self._get_id(call) + '.pk')
+        filename = os.path.join(self.base_path, self.config.get_id(call) + '.pk')
         return os.path.exists(filename) and os.path.isfile(filename)
 
     def get(self, call: CachedCall) -> Any:
@@ -115,10 +128,10 @@ class PickleCache(BaseCache):
         :param call:
         :return:
         """
-        filename = os.path.join(self.base_path, self._get_id(call) + '.pk')
+        filename = os.path.join(self.base_path, self.config.get_id(call) + '.pk')
         with open(filename, 'rb') as fp:
             data = pickle.load(fp)
-        assert call.func.__name__ == data['func']
+        assert call.func.__qualname__ == data['func']
         logger.debug(data)
 
         n_call = CachedCall(data['func'], data['args'], data['kwargs'], result=data['result'])
@@ -138,7 +151,7 @@ class PickleCache(BaseCache):
             raise Exception("not a picklable result...")
 
         return {
-            'func': call.func.__name__,
+            'func': call.func.__qualname__,
             'args': args,
             'kwargs': kwargs,
             'result': result,
@@ -150,7 +163,7 @@ class PickleCache(BaseCache):
         :param call:
         :return:
         """
-        filename = os.path.join(self.base_path, self._get_id(call) + '.pk')
+        filename = os.path.join(self.base_path, self.config.get_id(call) + '.pk')
         content = self._construct_content(call)
         with open(filename, 'wb') as fp:
             pickle.dump(content, fp)
@@ -160,11 +173,13 @@ class HistoryCache(PickleCache):
     """
     这个类只保存函数代码和运行结果，内容可以直接查看。
     """
+
     def __init__(self, base_path=None, tracking_files=None, cfg_path=None):
         self.tracking_files = tracking_files if tracking_files else []
         super().__init__(base_path, cfg_path)
 
     def __call__(self, func):
+        @wraps(func)
         def _get_result(*args, **kwargs):
             call = CachedCall(func, args, kwargs)
             self.set(call)
@@ -173,11 +188,11 @@ class HistoryCache(PickleCache):
         return _get_result
 
     def exists(self, call: CachedCall):
-        path = os.path.join(self.base_path, self._get_id(call))
+        path = os.path.join(self.base_path, self.config.get_id(call))
         return os.path.exists(path) and os.path.isdir(path)
 
     def set(self, call):
-        path = os.path.join(self.base_path, self._get_id(call))
+        path = os.path.join(self.base_path, self.config.get_id(call))
         os.makedirs(path, exist_ok=True)
         json_filename = os.path.join(path, 'function.json')
         content = self._construct_content(call)
--- test/sample1.py
+++ test/sample1.py
@@ -3,6 +3,9 @@ from FineCache import HistoryCache
 
 @HistoryCache()
 def run(a1, a2, k1="v1", k2="v2"):
+    """
+    example run function
+    """
     print(a1, a2, k1, k2)
     print(a1, "+ 1 =", a1 + 1)
     return a1 + 1, a2, k1, k2
@@ -10,3 +13,6 @@ def run(a1, a2, k1="v1", k2="v2"):
 
 res = run(3, a2=4, k1="v3")
 print(res)
+print(run.__qualname__)
+print(run.__doc__)
+print(run.__module__)
